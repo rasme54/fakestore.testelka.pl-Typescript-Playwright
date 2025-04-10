1f35787eb9b69ced3f546fac3a9a3cb822c777c5
Radoslaw Smektala <rsmektala54@gmail.com>
2025-04-10 17:46:01 +0200

    adding 5. test case

--- Tests/testScenario1_registerUser.spec.ts
+++ Tests/testScenario1_registerUser.spec.ts
@@ -3,6 +3,7 @@ import HomePage from "../PageObject/homePage";
 import LoginPage from "../PageObject/loginPage";
 import MyAccountPage from "../PageObject/myAccountPage";
 import Utils from "../PageObject/utils";
+import exisitingUser from "../fixtures/existingUser.json";
 import newUser from "../fixtures/newUser.json";
 
 test.describe("Scenario 1: Register User", () => {
@@ -15,15 +16,17 @@ test.describe("Scenario 1: Register User", () => {
     loginPage = new LoginPage(page);
     myAccountPage = new MyAccountPage(page);
     utils = new Utils(page);
+    await page.goto("/");
+    await page.waitForTimeout(2000);
+    await utils.turnOffPopUp("p[class='woocommerce-store-notice demo_store']", "a[class='woocommerce-store-notice__dismiss-link']");
   });
 
   test("TestCase1: Register User", async ({ page }) => {
     const user = newUser;
-    await page.goto("/");
+    test.setTimeout(60000);
     await homePage.selectLogInPage();
     await utils.isStringVisible("div[class='u-column2 col-2'] > h2", "Zarejestruj się");
-    await utils.turnOffPopUp("p[class='woocommerce-store-notice demo_store']", "a[class='woocommerce-store-notice__dismiss-link']");
-    await loginPage.fillSignUpEntryFields();
+    await loginPage.fillSignUpEntryFields(user);
     await myAccountPage.fillAccountInformation(user);
     await utils.isStringContians("div[class='woocommerce-message']", "Zmieniono szczegóły konta.");
     await myAccountPage.selectSubPage(4);
@@ -33,29 +36,24 @@ test.describe("Scenario 1: Register User", () => {
     await utils.isStringContians("div[class='woocommerce-message']", "Adres został zmieniony.");
     await myAccountPage.fillDeliveryAddressInformation(user);
     await utils.isStringContians("div[class='woocommerce-message']", "Adres został zmieniony.");
-    await myAccountPage.selectSubPage(1)
+    await myAccountPage.selectSubPage(1);
     await myAccountPage.deleteAccount();
   });
-  test("TestCase2: Delete User", async ({ page }) => {
-    await page.goto("/");
+
+  test("Test Case 5: Register User with existing email", async ({ page }) => {
+    const user = exisitingUser;
     await homePage.selectLogInPage();
     await utils.isStringVisible("div[class='u-column2 col-2'] > h2", "Zarejestruj się");
-    await utils.turnOffPopUp("p[class='woocommerce-store-notice demo_store']", "a[class='woocommerce-store-notice__dismiss-link']");
-    await loginPage.fillLogInEntryFields();
+    await loginPage.fillSignUpEntryFields(user);
+    const expectedString = "Konto jest już zarejestrowane w exisitngjohn.doe@email.com. Zaloguj się lub użyj innego adresu e-mail.";
+    await utils.isStringVisible("ul[class='woocommerce-error'] > li", expectedString);
+  });
+
+  test.skip("TestCase2: Delete User", async ({ page }) => {
+    const user = newUser;
+    await homePage.selectLogInPage();
+    await utils.isStringVisible("div[class='u-column2 col-2'] > h2", "Zarejestruj się");
+    await loginPage.fillLogInEntryFields(user);
     await myAccountPage.deleteAccount();
   });
 });
-
-// 6. Enter name and email address
-// 7. Click 'Signup' button
-// 8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
-// 9. Fill details: Title, Name, Email, Password, Date of birth
-// 10. Select checkbox 'Sign up for our newsletter!'
-// 11. Select checkbox 'Receive special offers from our partners!'
-// 12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
-// 13. Click 'Create Account button'
-// 14. Verify that 'ACCOUNT CREATED!' is visible
-// 15. Click 'Continue' button
-// 16. Verify that 'Logged in as username' is visible
-// 17. Click 'Delete Account' button
-// 18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
